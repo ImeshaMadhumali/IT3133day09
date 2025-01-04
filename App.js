3eafe421999ea30a973aceb8b00698d825977ec7
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-04 18:25:29 +0530

    Add PaperProvider component to the App

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <PaperProvider>
+      <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </PaperProvider>
+    
   );
 }
 
